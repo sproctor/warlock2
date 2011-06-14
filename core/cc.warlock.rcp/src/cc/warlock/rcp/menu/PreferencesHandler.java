cb6af5d43d94c0e3b3dcf03eeb4fe1271b3d2ffc
Sean Proctor <sproctor@gmail.com>
2011-06-13 21:08:04 -0400

    Use RCPUtil.openPreferences for Preferences menu item.

--- core/cc.warlock.rcp/src/cc/warlock/rcp/menu/PreferencesHandler.java
+++ core/cc.warlock.rcp/src/cc/warlock/rcp/menu/PreferencesHandler.java
@@ -23,32 +23,14 @@ package cc.warlock.rcp.menu;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.jface.preference.PreferenceDialog;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.ui.dialogs.PreferencesUtil;
 
-import cc.warlock.core.client.IWarlockClient;
-import cc.warlock.rcp.ui.client.WarlockClientAdaptable;
-import cc.warlock.rcp.views.GameView;
+import cc.warlock.rcp.util.RCPUtil;
 
 
 public class PreferencesHandler extends SimpleCommandHandler
 {	
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		
-		//IWarlockClient activeClient;
-		GameView inFocus = GameView.getGameViewInFocus();
-		//if (inFocus != null)
-		//{
-		IWarlockClient activeClient = inFocus.getWarlockClient();
-		//}
-		
-		PreferenceDialog dialog = PreferencesUtil.createPropertyDialogOn(Display.getDefault().getActiveShell(),
-				new WarlockClientAdaptable(activeClient), null, null, null);
-		
-		dialog.getTreeViewer().expandToLevel(2);
-		
-		int response = dialog.open();
+		RCPUtil.openPreferences(null);
 		
 		return null;
 	}
