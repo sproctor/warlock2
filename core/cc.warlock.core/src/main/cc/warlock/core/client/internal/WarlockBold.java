e0335a11f4cdae1b23df0883d04b6cd09cf7e681
sproctor <sproctor@gmail.com>
2012-10-02 23:46:47 -0400

    Added static bold and monospace styles to be used as helpers for places we just want one of those styles.

---
+++ core/cc.warlock.core/src/main/cc/warlock/core/client/internal/WarlockBold.java
@@ -0,0 +1,140 @@
+package cc.warlock.core.client.internal;
+
+import cc.warlock.core.client.IWarlockStyle;
+import cc.warlock.core.client.WarlockColor;
+
+public class WarlockBold implements IWarlockStyle {
+	private static IWarlockStyle instance = new WarlockBold();
+	
+	private WarlockColor foregroundColor = new WarlockColor(WarlockColor.DEFAULT_COLOR);
+	private WarlockColor backgroundColor = new WarlockColor(WarlockColor.DEFAULT_COLOR);
+	
+	public static IWarlockStyle getInstance() {
+		return instance;
+	}
+	
+	private WarlockBold() {
+		
+	}
+
+	@Override
+	public Runnable getAction() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WarlockColor getForegroundColor() {
+		return foregroundColor;
+	}
+
+	@Override
+	public WarlockColor getBackgroundColor() {
+		return backgroundColor;
+	}
+
+	@Override
+	public boolean isBold() {
+		return true;
+	}
+
+	@Override
+	public boolean isItalic() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isUnderline() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isFullLine() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isMonospace() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getName() {
+		return "bold";
+	}
+
+	@Override
+	public String getComponentName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getSound() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setAction(Runnable action) {
+		
+	}
+
+	@Override
+	public void setForegroundColor(WarlockColor color) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setBackgroundColor(WarlockColor color) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setFullLine(boolean fullLine) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setBold(boolean bold) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setItalic(boolean italic) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setUnderline(boolean underline) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setMonospace(boolean monospace) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setComponentName(String componentName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setSound(String sound) {
+		// TODO Auto-generated method stub
+		
+	}
+}
