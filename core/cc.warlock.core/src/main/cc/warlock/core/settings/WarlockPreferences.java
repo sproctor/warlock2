4ffcf397d159dc0411b4c0835d56f479c6438f1d
sproctor <sproctor@gmail.com>
2013-02-20 15:13:48 -0500

    Remove excessive syncs in loading preferences.

--- core/cc.warlock.core/src/main/cc/warlock/core/settings/WarlockPreferences.java
+++ core/cc.warlock.core/src/main/cc/warlock/core/settings/WarlockPreferences.java
@@ -11,27 +11,24 @@ public class WarlockPreferences implements IEclipsePreferences {
 	public WarlockPreferences(Preferences preferences) {
 		this.preferences = preferences;
 	}
-	
+
 	@Override
 	public void put(String key, String value) {
 		try {
 			preferences.sync();
-			preferences.put(key, value);
+			if(value == null)
+				preferences.remove(key);
+			else
+				preferences.put(key, value);
 			preferences.flush();
-		} catch(BackingStoreException e) {
+		} catch (BackingStoreException e) {
 			e.printStackTrace();
 		}
 	}
 
 	@Override
 	public String get(String key, String def) {
-		try {
-			preferences.sync();
-			return preferences.get(key, def);
-		} catch(BackingStoreException e) {
-			e.printStackTrace();
-			return null;
-		}
+		return preferences.get(key, def);
 	}
 
 	@Override
@@ -40,7 +37,7 @@ public class WarlockPreferences implements IEclipsePreferences {
 			preferences.sync();
 			preferences.remove(key);
 			preferences.flush();
-		} catch(BackingStoreException e) {
+		} catch (BackingStoreException e) {
 			e.printStackTrace();
 		}
 	}
@@ -58,20 +55,14 @@ public class WarlockPreferences implements IEclipsePreferences {
 			preferences.sync();
 			preferences.putInt(key, value);
 			preferences.flush();
-		} catch(BackingStoreException e) {
+		} catch (BackingStoreException e) {
 			e.printStackTrace();
 		}
 	}
 
 	@Override
 	public int getInt(String key, int def) {
-		try {
-			preferences.sync();
-			return preferences.getInt(key, def);
-		} catch(BackingStoreException e) {
-			e.printStackTrace();
-			throw new IllegalStateException();
-		}
+		return preferences.getInt(key, def);
 	}
 
 	@Override
@@ -80,20 +71,14 @@ public class WarlockPreferences implements IEclipsePreferences {
 			preferences.sync();
 			preferences.putLong(key, value);
 			preferences.flush();
-		} catch(BackingStoreException e) {
+		} catch (BackingStoreException e) {
 			e.printStackTrace();
 		}
 	}
 
 	@Override
 	public long getLong(String key, long def) {
-		try {
-			preferences.sync();
-			return preferences.getLong(key, def);
-		} catch(BackingStoreException e) {
-			e.printStackTrace();
-			throw new IllegalStateException();
-		}
+		return preferences.getLong(key, def);
 	}
 
 	@Override
@@ -102,20 +87,14 @@ public class WarlockPreferences implements IEclipsePreferences {
 			preferences.sync();
 			preferences.putBoolean(key, value);
 			preferences.flush();
-		} catch(BackingStoreException e) {
+		} catch (BackingStoreException e) {
 			e.printStackTrace();
 		}
 	}
 
 	@Override
 	public boolean getBoolean(String key, boolean def) {
-		try {
-			preferences.sync();
-			return preferences.getBoolean(key, def);
-		} catch(BackingStoreException e) {
-			e.printStackTrace();
-			throw new IllegalStateException();
-		}
+		return preferences.getBoolean(key, def);
 	}
 
 	@Override
@@ -124,20 +103,14 @@ public class WarlockPreferences implements IEclipsePreferences {
 			preferences.sync();
 			preferences.putFloat(key, value);
 			preferences.flush();
-		} catch(BackingStoreException e) {
+		} catch (BackingStoreException e) {
 			e.printStackTrace();
 		}
 	}
 
 	@Override
 	public float getFloat(String key, float def) {
-		try {
-			preferences.sync();
-			return preferences.getFloat(key, def);
-		} catch(BackingStoreException e) {
-			e.printStackTrace();
-			throw new IllegalStateException();
-		}
+		return preferences.getFloat(key, def);
 	}
 
 	@Override
@@ -146,20 +119,14 @@ public class WarlockPreferences implements IEclipsePreferences {
 			preferences.sync();
 			preferences.putDouble(key, value);
 			preferences.flush();
-		} catch(BackingStoreException e) {
+		} catch (BackingStoreException e) {
 			e.printStackTrace();
 		}
 	}
 
 	@Override
 	public double getDouble(String key, double def) {
-		try {
-			preferences.sync();
-			return preferences.getDouble(key, def);
-		} catch(BackingStoreException e) {
-			e.printStackTrace();
-			throw new IllegalStateException();
-		}
+		return preferences.getDouble(key, def);
 	}
 
 	@Override
@@ -168,48 +135,33 @@ public class WarlockPreferences implements IEclipsePreferences {
 			preferences.sync();
 			preferences.putByteArray(key, value);
 			preferences.flush();
-		} catch(BackingStoreException e) {
+		} catch (BackingStoreException e) {
 			e.printStackTrace();
 		}
 	}
 
 	@Override
 	public byte[] getByteArray(String key, byte[] def) {
-		try {
-			preferences.sync();
-			return preferences.getByteArray(key, def);
-		} catch(BackingStoreException e) {
-			e.printStackTrace();
-			throw new IllegalStateException();
-		}
+		return preferences.getByteArray(key, def);
 	}
 
 	@Override
 	public String[] keys() throws BackingStoreException {
-		preferences.sync();
 		return preferences.keys();
 	}
 
 	@Override
 	public String[] childrenNames() throws BackingStoreException {
-		preferences.sync();
 		return preferences.childrenNames();
 	}
 
 	@Override
 	public Preferences parent() {
-		try {
-			preferences.sync();
-			return new WarlockPreferences(preferences.parent());
-		} catch(BackingStoreException e) {
-			e.printStackTrace();
-			return null;
-		}
+		return new WarlockPreferences(preferences.parent());
 	}
 
 	@Override
 	public boolean nodeExists(String pathName) throws BackingStoreException {
-		preferences.sync();
 		return preferences.nodeExists(pathName);
 	}
 
@@ -236,30 +188,29 @@ public class WarlockPreferences implements IEclipsePreferences {
 
 	@Override
 	public void addNodeChangeListener(INodeChangeListener listener) {
-		if(preferences instanceof IEclipsePreferences) {
-			((IEclipsePreferences)preferences).addNodeChangeListener(listener);
+		if (preferences instanceof IEclipsePreferences) {
+			((IEclipsePreferences) preferences).addNodeChangeListener(listener);
 		}
 	}
 
 	@Override
 	public void removeNodeChangeListener(INodeChangeListener listener) {
-		if(preferences instanceof IEclipsePreferences) {
-			((IEclipsePreferences)preferences).removeNodeChangeListener(listener);
+		if (preferences instanceof IEclipsePreferences) {
+			((IEclipsePreferences) preferences).removeNodeChangeListener(listener);
 		}
 	}
 
 	@Override
 	public void addPreferenceChangeListener(IPreferenceChangeListener listener) {
-		if(preferences instanceof IEclipsePreferences) {
-			((IEclipsePreferences)preferences).addPreferenceChangeListener(listener);
+		if (preferences instanceof IEclipsePreferences) {
+			((IEclipsePreferences) preferences).addPreferenceChangeListener(listener);
 		}
 	}
 
 	@Override
-	public void removePreferenceChangeListener(
-			IPreferenceChangeListener listener) {
-		if(preferences instanceof IEclipsePreferences) {
-			((IEclipsePreferences)preferences).removePreferenceChangeListener(listener);
+	public void removePreferenceChangeListener(IPreferenceChangeListener listener) {
+		if (preferences instanceof IEclipsePreferences) {
+			((IEclipsePreferences) preferences).removePreferenceChangeListener(listener);
 		}
 	}
 
@@ -277,10 +228,9 @@ public class WarlockPreferences implements IEclipsePreferences {
 	}
 
 	@Override
-	public void accept(IPreferenceNodeVisitor visitor)
-			throws BackingStoreException {
-		if(preferences instanceof IEclipsePreferences) {
-			((IEclipsePreferences)preferences).accept(visitor);
+	public void accept(IPreferenceNodeVisitor visitor) throws BackingStoreException {
+		if (preferences instanceof IEclipsePreferences) {
+			((IEclipsePreferences) preferences).accept(visitor);
 		}
 	}
 
@@ -288,28 +238,28 @@ public class WarlockPreferences implements IEclipsePreferences {
 		try {
 			preferences.sync();
 			Preferences newPrefs = preferences.parent().node(path);
-			for(String key : preferences.keys()) {
+			for (String key : preferences.keys()) {
 				newPrefs.put(key, preferences.get(key, null));
 			}
-			for(String name : preferences.childrenNames()) {
+			for (String name : preferences.childrenNames()) {
 				new WarlockPreferences(preferences.node(name)).reparent(newPrefs);
 			}
 			preferences.flush();
-		} catch(BackingStoreException e) {
+		} catch (BackingStoreException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private void reparent(Preferences parent) {
 		try {
 			Preferences newPrefs = parent.node(preferences.name());
-			for(String key : preferences.keys()) {
+			for (String key : preferences.keys()) {
 				newPrefs.put(key, preferences.get(key, null));
 			}
-			for(String name : preferences.childrenNames()) {
+			for (String name : preferences.childrenNames()) {
 				new WarlockPreferences(preferences.node(name)).reparent(newPrefs);
 			}
-		} catch(BackingStoreException e) {
+		} catch (BackingStoreException e) {
 			e.printStackTrace();
 		}
 	}
